de8bc1f18d6275ad8ccf46313dce3052af0ac2bf
pgmiras <pgmiras@students.cs.ubc.ca>
2024-10-01 23:30:36 -0700

    Added specification of GroceryItem class

---
+++ src/main/model/GroceryItem.java
@@ -0,0 +1,37 @@
+package model;
+
+public class GroceryItem {
+    private String name;
+    private int quantity;
+    private String category;
+
+    // REQUIRES: name has non-zero length, quantity > 0, category has non-zero length
+    // EFFECTS: constructs a grocery item with given name, quantity, and category
+    public GroceryItem(String n, int q, String c) {
+        // TODO GroceryItem constructor
+    }
+
+    public String getName(){
+        return ""; // TODO getName
+    }
+
+    public int getQuantity(){
+        return 0; // TODO getQuantity
+    }
+
+    public String getCategory(){
+        return ""; // TODO getCategory
+    }
+
+    public void setName(){
+        // TODO setName
+    }
+
+    public void setQuantity(){
+        // TODO setQuantity
+    }
+
+    public void setCategory(){
+        // TODO setCategory
+    }
+}
