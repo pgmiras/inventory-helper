eca6aeac81917756d978c81e3db7a3bd282901f5
pgmiras <pgmiras@students.cs.ubc.ca>
2024-10-01 23:40:34 -0700

    Added tests for GroceryItem class

---
+++ src/test/model/GroceryItemTest.java
@@ -0,0 +1,23 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GroceryItemTest {
+    private GroceryItem testGroceryItem;
+
+    @BeforeEach
+    void runBefore() {
+        testGroceryItem = new GroceryItem("milk", 1, "dairy");
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("milk", testGroceryItem.getName());
+        assertEquals(1, testGroceryItem.getQuantity());
+        assertEquals("dairy", testGroceryItem.getCategory());
+    }
+}
