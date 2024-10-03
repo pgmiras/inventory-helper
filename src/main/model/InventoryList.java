5ee79d1a6900248cb8be66c97735f38403536780
pgmiras <pgmiras@students.cs.ubc.ca>
2024-10-03 14:49:41 -0700

    Updated specification for InventoryList to use HashSet instead of ArrayList, Added implementation for InventoryList

--- src/main/model/InventoryList.java
+++ src/main/model/InventoryList.java
@@ -1,34 +1,34 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 
 public class InventoryList {
-    private List<GroceryItem> inventory;
+    private HashSet<GroceryItem> inventory;
 
     // EFFECTS: constructs user's inventory with no items
     public InventoryList() {
-        // TODO stub
+        inventory = new HashSet<GroceryItem>();
     }
 
     // MODIFIES: this
     // EFFECTS: adds given grocery item into inventory list
     public void addItem(GroceryItem groceryItem) {
-        // TODO stub
+        inventory.add(groceryItem);
     }
 
     // REQUIRES: given grocery item is already in the list
     // MODIFIES: this
     // EFFECTS: removes given grocery item from inventory list
     public void removeItem(GroceryItem groceryItem) {
-        // TODO stub
+        inventory.remove(groceryItem);
     }
 
     // REQUIRES: given grocery item is already in the list, amount > 0
     // MODIFIES: groceryItem
     // EFFECTS: increases groceryItem's quantity in inventory by given amount
     public void increaseItemQuantity(GroceryItem groceryItem, int amount){
-        // TODO stub
+        int newQuantity = groceryItem.getQuantityInInventory() + amount;
+        groceryItem.setQuantityInInventory(newQuantity);
     }
 
     // REQUIRES: given grocery item is already in the list, and groceryItem.getQuantityInInventory() >= amount > 0
@@ -36,10 +36,14 @@ public class InventoryList {
     // EFFECTS: decreases groceryItem's quantity in inventory by given amount;
     // if groceryItem's new quantity is 0, remove this item from inventory
     public void decreaseItemQuantity(GroceryItem groceryItem, int amount){
-        // TODO stub
+        int newQuantity = groceryItem.getQuantityInInventory() - amount;
+        groceryItem.setQuantityInInventory(newQuantity);
+        if (newQuantity == 0) {
+            this.removeItem(groceryItem);
+        }
     }
 
-    public ArrayList<GroceryItem> getInventoryList() {
-        return null; // TODO stub
+    public HashSet<GroceryItem> getInventoryList() {
+        return inventory;
     }
 }
