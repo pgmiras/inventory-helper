82198a4b2e637bf65311feaac7e692fcd31e3a1c
pgmiras <pgmiras@students.cs.ubc.ca>
2024-10-03 21:38:53 -0700

    Added implementation for ShoppingList class

--- src/main/model/ShoppingList.java
+++ src/main/model/ShoppingList.java
@@ -7,27 +7,28 @@ public class ShoppingList {
 
     // EFFECTS: constructs user's shopping list with no items
     public ShoppingList() {
-        // TODO 
+        shoppingList = new HashSet<GroceryItem>();
     }
 
     // MODIFIES: this
     // EFFECTS: adds given grocery item into shopping list
     public void addItem(GroceryItem groceryItem) {
-        // TODO
+        shoppingList.add(groceryItem);
     }
 
     // REQUIRES: given grocery item is already in the list
     // MODIFIES: this
     // EFFECTS: removes given grocery item from shopping list
     public void removeItem(GroceryItem groceryItem) {
-        // TODO
+        shoppingList.remove(groceryItem);
     }
 
     // REQUIRES: given grocery item is already in the list, amount > 0
     // MODIFIES: groceryItem
     // EFFECTS: increases groceryItem's quantity in shopping list by given amount
     public void increaseItemQuantity(GroceryItem groceryItem, int amount){
-        // TODO
+        int newQuantity = groceryItem.getQuantityInShoppingList() + amount;
+        groceryItem.setQuantityInShoppingList(newQuantity);
     }
 
     // REQUIRES: given grocery item is already in the list, and groceryItem.getQuantityInShoppingList() >= amount > 0
@@ -35,10 +36,14 @@ public class ShoppingList {
     // EFFECTS: decreases groceryItem's quantity in shopping list by given amount;
     // if groceryItem's new quantity is 0, remove this item from shopping list
     public void decreaseItemQuantity(GroceryItem groceryItem, int amount){
-        // TODO
+        int newQuantity = groceryItem.getQuantityInShoppingList() - amount;
+        groceryItem.setQuantityInShoppingList(newQuantity);
+        if (newQuantity == 0) {
+            this.removeItem(groceryItem);
+        }
     }
 
     public HashSet<GroceryItem> getShoppingList() {
-        return null; // TODO
+        return shoppingList;
     }
 }
