2afae2484618869b5371d3a02db6f442c83e1ad8
pgmiras <pgmiras@students.cs.ubc.ca>
2024-10-03 22:11:08 -0700

    Added specification for InventoryApp

---
+++ src/main/ui/InventoryApp.java
@@ -0,0 +1,61 @@
+package ui;
+
+// Inventory application
+public class InventoryApp {
+
+    // EFFECTS: runs the inventory application
+    public InventoryApp() {
+        runInventoryApp();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user input
+    public void runInventoryApp() {
+        // TODO
+    }
+
+    // EFFECTS: display menu of options to user
+    public void displayMenu() {
+        // TODO
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user command
+    public void processCommand() {
+        // TODO
+    }
+
+    // EFFECTS: display user's inventory
+    public void displayInventory() {
+        // TODO
+    }
+
+    // MODIFIES: this, inventory
+    // EFFECTS: allows user to add items to inventory
+    public void addToInventory() {
+        // TODO
+    }
+
+    // MODIFIES: this, inventory
+    // EFFECTS: allows user to modify items already inside inventory
+    public void modifyInventory() {
+        // TODO
+    }
+
+    // EFFECTS: display user's shopping list
+    public void displayShoppingList() {
+        // TODO
+    }
+
+    // MODIFIES: this, shopping
+    // EFFECTS: allows user to add items to inventory
+    public void addToShoppingList() {
+        // TODO
+    }
+
+    // MODIFIES: this, shopping
+    // EFFECTS: allows user to modify items already inside shopping list
+    public void modifyShoppingList() {
+        // TODO
+    }
+}
