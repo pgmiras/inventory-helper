fb0f2bf4c7e75b352c05b210fc1eec7d13d49342
pgmiras <pgmiras@students.cs.ubc.ca>
2024-10-07 22:36:43 -0700

    Added abstract class GroceryList which InventoryList and ShoppingList classes extend

--- src/main/model/InventoryList.java
+++ src/main/model/InventoryList.java
@@ -3,30 +3,17 @@ package model;
 import java.util.HashSet;
 
 // Represents user's inventory
-public class InventoryList {
-    private HashSet<GroceryItem> inventory;
-
+public class InventoryList extends GroceryList {
+    
     // EFFECTS: constructs user's inventory with no items
     public InventoryList() {
-        inventory = new HashSet<GroceryItem>();
-    }
-
-    // MODIFIES: this
-    // EFFECTS: adds given grocery item into inventory list
-    public void addItem(GroceryItem groceryItem) {
-        inventory.add(groceryItem);
-    }
-
-    // REQUIRES: given grocery item is already in the list
-    // MODIFIES: this
-    // EFFECTS: removes given grocery item from inventory list
-    public void removeItem(GroceryItem groceryItem) {
-        inventory.remove(groceryItem);
+        super();
     }
 
     // REQUIRES: given grocery item is already in the list, amount > 0
     // MODIFIES: groceryItem
     // EFFECTS: increases groceryItem's quantity in inventory by given amount
+    @Override
     public void increaseItemQuantity(GroceryItem groceryItem, int amount){
         int newQuantity = groceryItem.getQuantityInInventory() + amount;
         groceryItem.setQuantityInInventory(newQuantity);
@@ -36,15 +23,12 @@ public class InventoryList {
     // MODIFIES: groceryItem
     // EFFECTS: decreases groceryItem's quantity in inventory by given amount;
     // if groceryItem's new quantity is 0, remove this item from inventory
+    @Override
     public void decreaseItemQuantity(GroceryItem groceryItem, int amount){
         int newQuantity = groceryItem.getQuantityInInventory() - amount;
         groceryItem.setQuantityInInventory(newQuantity);
         if (newQuantity == 0) {
             this.removeItem(groceryItem);
         }
-    }
-
-    public HashSet<GroceryItem> getInventoryList() {
-        return inventory;
     }
 }
--- src/main/model/ShoppingList.java
+++ src/main/model/ShoppingList.java
@@ -3,30 +3,17 @@ package model;
 import java.util.HashSet;
 
 // Represents user's shopping list
-public class ShoppingList {
-    private HashSet<GroceryItem> shoppingList;
+public class ShoppingList extends GroceryList{
 
     // EFFECTS: constructs user's shopping list with no items
     public ShoppingList() {
-        shoppingList = new HashSet<GroceryItem>();
-    }
-
-    // MODIFIES: this
-    // EFFECTS: adds given grocery item into shopping list
-    public void addItem(GroceryItem groceryItem) {
-        shoppingList.add(groceryItem);
-    }
-
-    // REQUIRES: given grocery item is already in the list
-    // MODIFIES: this
-    // EFFECTS: removes given grocery item from shopping list
-    public void removeItem(GroceryItem groceryItem) {
-        shoppingList.remove(groceryItem);
+        super();
     }
 
     // REQUIRES: given grocery item is already in the list, amount > 0
     // MODIFIES: groceryItem
     // EFFECTS: increases groceryItem's quantity in shopping list by given amount
+    @Override
     public void increaseItemQuantity(GroceryItem groceryItem, int amount){
         int newQuantity = groceryItem.getQuantityInShoppingList() + amount;
         groceryItem.setQuantityInShoppingList(newQuantity);
@@ -36,15 +23,12 @@ public class ShoppingList {
     // MODIFIES: groceryItem
     // EFFECTS: decreases groceryItem's quantity in shopping list by given amount;
     // if groceryItem's new quantity is 0, remove this item from shopping list
+    @Override
     public void decreaseItemQuantity(GroceryItem groceryItem, int amount){
         int newQuantity = groceryItem.getQuantityInShoppingList() - amount;
         groceryItem.setQuantityInShoppingList(newQuantity);
         if (newQuantity == 0) {
             this.removeItem(groceryItem);
         }
-    }
-
-    public HashSet<GroceryItem> getShoppingList() {
-        return shoppingList;
     }
 }
