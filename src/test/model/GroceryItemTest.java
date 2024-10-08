6d55cfd0e2b403f84b8273e21d910664e8adc48c
pgmiras <pgmiras@students.cs.ubc.ca>
2024-10-07 23:10:27 -0700

    Added tests for GroceryItem class

--- src/test/model/GroceryItemTest.java
+++ src/test/model/GroceryItemTest.java
@@ -20,4 +20,32 @@ public class GroceryItemTest {
         assertEquals(0, testGroceryItem.getQuantityInShoppingList());
         assertEquals("dairy", testGroceryItem.getCategory());
     }
+
+    @Test
+    public void testIncreaseQuantityInInventory(int amount){
+        testGroceryItem.increaseQuantityInInventory(3);
+        assertEquals(3, testGroceryItem.getQuantityInInventory());
+    }
+
+    @Test  
+    public void testDecreaseQuantityInInventoryList(int amount){
+        testGroceryItem.increaseQuantityInInventory(5);
+        assertEquals(5, testGroceryItem.getQuantityInInventory());
+        testGroceryItem.decreaseQuantityInInventory(3);
+        assertEquals(2, testGroceryItem.getQuantityInInventory());
+    }
+
+    @Test
+    public void testIncreaseQuantityInShoppingList(int amount){
+        testGroceryItem.increaseQuantityInShoppingList(5);
+        assertEquals(5, testGroceryItem.getQuantityInShoppingList());
+    }
+
+    @Test  
+    public void testDecreaseQuantityInShoppingList(int amount){
+        testGroceryItem.increaseQuantityInShoppingList(6);
+        assertEquals(6, testGroceryItem.getQuantityInShoppingList());
+        testGroceryItem.decreaseQuantityInShoppingList(4);
+        assertEquals(2, testGroceryItem.getQuantityInShoppingList());
+    }
 }
