4faf658178bb4aca158de7fdf874978b350a40ac
pgmiras <pgmiras@students.cs.ubc.ca>
2024-10-08 20:15:14 -0700

    Updated specifications in GroceryList to remove methods only modifying groceryItem

--- src/main/model/GroceryList.java
+++ src/main/model/GroceryList.java
@@ -2,7 +2,7 @@ package model;
 
 import java.util.HashSet;
 
-public abstract class GroceryList {
+public class GroceryList {
     protected HashSet<GroceryItem> groceryList;
 
     // EFFECTS: constructs a grocery list with no items
@@ -26,19 +26,13 @@ public abstract class GroceryList {
     // REQUIRES: given name is the name of an item that exists in the list
     // EFFECTS: returns item with the given name
     public GroceryItem getItem(String name){
-        return null; // TODO
+        for (GroceryItem groceryItem : groceryList) {
+            if (name.equals(groceryItem.getName())) {
+                    return groceryItem;
+            }
+        }
+        return null;
     }
-
-    // REQUIRES: given grocery item is already in the list, amount > 0
-    // MODIFIES: groceryItem
-    // EFFECTS: increases groceryItem's quantity in list by given amount
-    public abstract void increaseItemQuantity(GroceryItem groceryItem, int amount);
-
-    // REQUIRES: given grocery item is already in the list, and groceryItem.getQuantityInInventory() >= amount > 0
-    // MODIFIES: groceryItem
-    // EFFECTS: decreases groceryItem's quantity in list by given amount;
-    // if groceryItem's new quantity is 0, remove this item from list
-    public abstract void decreaseItemQuantity(GroceryItem groceryItem, int amount);
 
     public HashSet<GroceryItem> getGroceryList() {
         return groceryList;
