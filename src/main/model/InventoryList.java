4cbb4400c1b4f11f18b0628bcc26a91ff30a3760
pgmiras <pgmiras@students.cs.ubc.ca>
2024-11-11 21:44:43 -0800

    Added specifications for InventoryList and ShoppingList classes

---
+++ src/main/model/InventoryList.java
@@ -0,0 +1,11 @@
+package model;
+
+// Grocery list for items user already has
+public class InventoryList extends GroceryList {
+    
+    // EFFECTS: constructs a grocery list of list type inventory
+    public InventoryList() {
+        // TODO
+    }
+
+}
---
+++ src/main/model/ShoppingList.java
@@ -0,0 +1,11 @@
+package model;
+
+// Grocery list for items user has yet to purchase
+public class ShoppingList extends GroceryList {
+
+    // EFFECTS: constructs a grocery list of list type shopping list
+    public ShoppingList() {
+        // TODO
+    }
+
+}
